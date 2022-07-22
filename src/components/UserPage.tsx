0ae46b7c1aa6d1817d73e44f2c706ab4498d8016
KhusniddinovUz <khusniddinovuz@gmail.com>
2022-07-22 16:07:47 +0500

    Create user page for detailed information

---
+++ src/components/UserPage.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Container from '@mui/material/Container';
+
+
+const UserPage: React.FC = () => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+};
+
+export default UserPage;
