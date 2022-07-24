46cabdcf561d29a2da1e79b78facfa2e0a31f5e7
KhusniddinovUz <khusniddinovuz@gmail.com>
2022-07-24 17:11:09 +0500

    Activate loading, show loading animation when user is requesting

--- src/redux/action-creaters/auth.ts
+++ src/redux/action-creaters/auth.ts
@@ -1,22 +1,30 @@
 import axios from "axios";
 import {AuthActionTypes} from "../action-types/auth";
-import {AuthAction} from "../actions/auth";
+import {LoadingTypes} from "../action-types/loading";
 import {Dispatch} from "redux";
 
 export const login = (data: { email: string, password: string }) => {
-    return (dispatch: Dispatch<AuthAction>) => {
-        let token = "";
+    return (dispatch: Dispatch) => {
+        dispatch({
+            type: LoadingTypes.LOADING_STARTED
+        });
         axios.post(`https://reqres.in/api/login`, data).then(response => {
-            token = response.data.token;
+            const token = response.data.token;
             dispatch({
                 type: AuthActionTypes.LOGIN_SUCCESS,
                 payload: token
-            })
+            });
+            dispatch({
+                type: LoadingTypes.LOADING_ENDED
+            });
         }).catch(error => {
             console.log(error.response.data.error);
             dispatch({
                 type: AuthActionTypes.LOGIN_FAIL
-            })
+            });
+            dispatch({
+                type: LoadingTypes.LOADING_ENDED
+            });
         });
 
     }
@@ -24,19 +32,28 @@ export const login = (data: { email: string, password: string }) => {
 
 
 export const register = (data: { email: string, password: string }) => {
-    return (dispatch: Dispatch<AuthAction>) => {
-        let token = "";
+    return (dispatch: Dispatch) => {
+        dispatch({
+            type: LoadingTypes.LOADING_STARTED
+        });
         axios.post(`https://reqres.in/api/register`, data).then(response => {
-            token = response.data.token;
+            const token = response.data.token;
             dispatch({
                 type: AuthActionTypes.REGISTER_SUCCESS,
                 payload: token
-            })
+            });
+
+            dispatch({
+                type: LoadingTypes.LOADING_ENDED
+            });
         }).catch(error => {
             console.log(error.response.data.error);
             dispatch({
                 type: AuthActionTypes.REGISTER_FAIL
-            })
+            });
+            dispatch({
+                type: LoadingTypes.LOADING_ENDED
+            });
         });
 
     }
